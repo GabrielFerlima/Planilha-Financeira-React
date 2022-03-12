a96e270304b0003e6298185d9094de7507c5461b
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-03-12 16:00:52 -0300

    Styled components instalado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import styled from 'styled-components'
+
+const Title = styled.h1 `
+color: #8257e6;
+`
 
 export function App() {
   return (
-    <div className="App">
-     <h1>Hello World!</h1>
+    <div className='App'>
+      <Title>Hello World!</Title>
     </div>
-  );
+  )
 }
-
