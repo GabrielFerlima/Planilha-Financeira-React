c6c8da1293c4be7f4383a543f3128591a900c64c
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-03-16 22:06:19 -0300

    Configurado miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
 import {Container} from './styles'
+import {createServer} from 'miragejs'
+
+
 
 
 export function TransactionsTable() {
+ useEffect(()=> {
+    fetch('http://localhost:3000/api/transactions')
+    .then(response => response.json())
+    .then(data => console.log(data))
+ }, []);
+
+
     return (
         <Container> 
 
