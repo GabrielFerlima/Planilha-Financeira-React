6ac0061ef9d2bd4de00cdc913f2616bb99caa813
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-03-17 12:00:05 -0300

    Configurado Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import {Container, Content} from './styles'
+import Modal from 'react-modal'
 
-export function Header() {
+
+interface HeaderProbs {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProbs) {
+
+  
     return (
         <Container>
             <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">
+
+            <button type="button" onClick={onOpenNewTransactionModal}>
                 Nova transação
             </button>
+
+
             </Content>
         </Container>
         
