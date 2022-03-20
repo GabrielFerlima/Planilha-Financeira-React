4b8606e8c89e85f9fbc3fd02d878c9865c357dbe
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-03-20 13:34:40 -0300

    Listando transações e seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,15 +1,26 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import {Container} from './styles'
 import {api} from '../../services/api'
 
 
+interface Transaction {
+    id: number;
+    title: string; 
+    amount: number;
+    type: string;
+    category: string;
+    createAt: string;
+}
 
 
 
 export function TransactionsTable() {
+    
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
  useEffect(()=> {
     api.get('transactions')
-    .then(response => console.log(response.data))
+    .then(response => setTransactions(response.data.transactions))
  }, []);
 
 
@@ -27,20 +38,17 @@ export function TransactionsTable() {
             </thead>
 
             <tbody>
-                <tr>
-                    <td>Desenvolvimento de website</td>
-                    <td className="deposit">12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>16/03/2022</td>
+            {transactions.map(transaction => { 
+                return (
+                    <tr key={transaction.id}>
+                    <td>{transaction.title}</td>
+                    <td className={transaction.type}>{transaction.amount}</td>
+                    <td>{transaction.category}</td>
+                    <td>{transaction.createAt}</td>
                 </tr>
 
-                <tr>
-                    <td>Aluguel</td>
-                    <td className="withdraw">-1.100</td>
-                    <td>Casa</td>
-                    <td>19/03/2022</td>
-                </tr>
-
+                )
+             })}   
                 
             </tbody>
         </table>
--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,33 @@ createServer({
   models: {
     transaction: Model
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        { 
+          id: 1, 
+          title: 'Freelancer de website', 
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createAt: new Date('2022-03-20 09:00:00'),
+        },
+
+        { 
+          id: 2, 
+          title: 'Aluguel', 
+          type: 'withdraw',
+          category: 'Casa',
+          amount: -1100,
+          createAt: new Date('2022-03-30 11:00:00'),
+        },
+
+
+      ]
+    })
+  },
+
   routes() {
 
     this.namespace = 'api';
